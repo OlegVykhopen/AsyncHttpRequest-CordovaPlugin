2b8684689a1164bc48ca2be951ba5dc67950a015
ovyhopen <oleh.vyhopen@ukrinsoft.com>
2016-09-21 16:47:17 +0300

    correct caling plugin form js bridge

--- www/AsyncHttpRequest.js
+++ www/AsyncHttpRequest.js
@@ -1,9 +1,8 @@
-cordova.define("cordova-plugin-asynchttprequest.AsyncHttpRequest", function(require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-    exports.loadDataFromUrl = function(success,error,str) {
-        if (loader && loader.length>0) loader.show();
-        cordova.exec(success, error, "AsyncHttpRequest", "loadDataFromURL", [str]);
-    };
+function loadDataFromUrl(success,error,str) {
+    if (loader && loader.length>0) loader.show();
+    cordova.exec(success, error, "AsyncHttpRequest", "loadDataFromURL", [str]);
+};
 
-});
+
