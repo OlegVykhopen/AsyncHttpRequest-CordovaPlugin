92f8a946b84628c6a0da31d0b89655729d7f52fd
ovyhopen <oleh.vyhopen@ukrinsoft.com>
2016-09-21 16:16:40 +0300

    addded imports and dependecies

--- src/android/AsyncHttpRequest.java
+++ src/android/AsyncHttpRequest.java
@@ -1,80 +1,66 @@
-package cordova-plugin-asynchttprequest;
-
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
+import android.util.Log;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.JsonHttpResponseHandler;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaActivity;
-import org.apache.http.Header;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import java.io.UnsupportedEncodingException;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class AsyncHttpRequest extends CordovaPlugin {
 
-  private CordovaActivity mCRDActivity;
-  private CallbackContext mCallbackContext;
-  private AsyncHttpClient mClient;
-  private String mRequestUrl;
+    private CordovaActivity mCRDActivity;
+    private CallbackContext mCallbackContext;
+    private AsyncHttpClient mClient;
+    private String mRequestUrl;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (paramString.equals("loadDataFromURL"))
-            {
-              String str2 = paramJSONArray.getString(0);
-              this.mCallbackContext = paramCallbackContext;
-              this.mRequestUrl = str2;
-              this.mClient = new AsyncHttpClient();
-              loadDataFromURL();
-              return true;
-            }
+        if (action.equals("loadDataFromURL")) {
+            String str2 = args.getString(0);
+            this.mCallbackContext = callbackContext;
+            this.mRequestUrl = str2;
+            this.mClient = new AsyncHttpClient();
+            loadDataFromURL();
+            return true;
+        }
         return false;
     }
 
-    public void loadDataFromURL()
-      {
-        if ((this.mRequestUrl != null) && (this.mRequestUrl.length() > 0))
-        {
-        	Log.e("LoadDataFromUrl",this.mRequestUrl);
-          this.mClient.get(this.mRequestUrl, new AsyncHttpResponseHandler()
-          {
+    public void loadDataFromURL() {
+        if ((this.mRequestUrl != null) && (this.mRequestUrl.length() > 0)) {
+            Log.e("LoadDataFromUrl", this.mRequestUrl);
+            this.mClient.get(this.mRequestUrl, new AsyncHttpResponseHandler() {
 
-    		@Override
-    		public void onFailure(int arg0, Header[] arg1, byte[] arg2,
-    				Throwable arg3) {
-    			// TODO Auto-generated method stub
+                @Override
+                public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                    try {
+                        JSONObject localJSONObject = (JSONObject) new JSONTokener(new String(bytes, "UTF-8")).nextValue();
+                        AsyncHttpRequest.this.mCallbackContext.success(localJSONObject);
+                    } catch (JSONException localJSONException) {
+                        localJSONException.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
 
-    		}
+                @Override
+                public void onFailure(int i, org.apache.http.Header[] headers, byte[] bytes, Throwable throwable) {
 
-    		@Override
-    		public void onSuccess(int arg0, Header[] arg1, byte[] paramAnonymousJSONObject) {
-    			// TODO Auto-generated method stub
-    			try
-    	          {
-    	            JSONObject localJSONObject = (JSONObject)new JSONTokener(new String(paramAnonymousJSONObject, "UTF-8")).nextValue();
-    	            KfarAndroidExtentionPlugin.this.mCallbackContext.success(localJSONObject);
-    	            return;
-    	          }
-    	          catch (JSONException localJSONException)
-    	          {
-    	            localJSONException.printStackTrace();
-    	          } catch (UnsupportedEncodingException e) {
-    				// TODO Auto-generated catch block
-    				e.printStackTrace();
-    			}
+                }
 
-    		}
 
-          });
-          return;
+            });
+            return;
         }
         this.mCallbackContext.error("Expected one non-empty string argument.");
-      }
+    }
 }
